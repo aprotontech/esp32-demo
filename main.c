86980ed4d7bcb2eb45f1d478f23e597b66165e47
kuper <kuper@aproton.tech>
2021-10-30 11:19:44 +0800

    update wifi

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 
 #include "quark/quark.h"
 
-int wifi_setuped = 0;
+extern int mock_virtual_device(char* env, char* app_id, char* app_secret, int test_time_sec);
 
 void app_main(void)
 {
@@ -24,23 +26,18 @@ void app_main(void)
     printf("silicon revision %d, ", chip_info.revision);
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-    
-    if (wifi_setuped == 0) {
-        wifi_setuped = 1;
-        for (int i = 10; i >= 0; i--) {
-            printf("Start to connect to wifi in %d seconds...\n", i);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        rc_set_wifi("kog_2.4G", "huxiaolong@2018");
-    }        
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");     
+    rc_sleep(1000);
 
-    for (int i = 60; i >= 0; i--) {
-        if (i % 10 == 0) {
-            printf("Restarting in %d seconds...\n", i);
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+
+    mock_virtual_device("test", "app", "secret", 10 * 60);
     printf("kog Restarting now.\n");
     fflush(stdout);
     esp_restart();
